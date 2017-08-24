7b4eaf10c720415fd8ffdb6c70153e469e39dabf
Jorge Bo <jorge.bo@almundo.com>
2017-08-24 13:01:51 -0300

    add sleuth

--- src/main/java/customer/CustomerConfiguration.java
+++ src/main/java/customer/CustomerConfiguration.java
@@ -1,6 +1,8 @@
 package customer;
 
 
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +10,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("customer")
 public class CustomerConfiguration {
 
-
+    @Bean
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
+    }
 }
